0852ea8d18bb49e28d1042f4c5a057d7315c503e
Lucas Antonio <dev.lucasbarbo@gmail.com>
2025-01-31 16:37:19 -0300

    Criando a classe do database

--- database.py
+++ database.py
@@ -1,7 +1,33 @@
 import psycopg2
 
-conn = psycopg2.connect(database = "postgres",
-                        user = "postgres",
-                        host = 'localhost',
-                        password = "postgres",
-                        port = 5433)
+class ConexaoDB():
+    def __init__(self, 
+                 database = "postgres", 
+                 user = "postgres", 
+                 host = "localhost", 
+                 password = "postgres", 
+                 port = 5433):
+        self.database = database
+        self.user = user
+        self.host = host
+        self.password = password
+        self.port = port
+
+    def conectar(self):
+        self.con = psycopg2.connect(database = self.database,
+                                    user = self.user,
+                                    host = self.host,
+                                    password = self.password,
+                                    port = self.port)
+        
+        self.cur = self.con.cursor()
+
+    def desconectar(self):
+        self.con.close()
+
+
+# conn = psycopg2.connect(database = "postgres",
+#                         user = "postgres",
+#                         host = 'localhost',
+#                         password = "postgres",
+#                         port = 5433)
