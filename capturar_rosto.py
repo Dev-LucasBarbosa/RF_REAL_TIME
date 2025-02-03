6bd3bd138c1997dc3e6dff12067ff9e04d542762
Lucas Antonio <dev.lucasbarbo@gmail.com>
2025-02-03 15:47:56 -0300

    Criando classes no arquivo capturar_rosto.py e alteração no arquivo main.py

--- capturar_rosto.py
+++ capturar_rosto.py
@@ -1,49 +1,45 @@
 import cv2
-import psycopg2
 import face_recognition
-import numpy
-from database import conn 
-from medidas_rosto import buscar_medidas_banco
+from medidas_rosto import BaseRostos
 
-classificador = cv2.CascadeClassifier(
-     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
+class ReconhecimentoFacial:
+    def __init__(self):
+        self.classificador = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        self.captura = cv2.VideoCapture(0)
+        self.captura.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        self.captura.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
-captura = cv2.VideoCapture(0)
+        self.db = BaseRostos()
+        self.nomes_salvos, self.medidas_salvas = self.db.buscar_medidas_banco()
+        self.db.fechar_conexao()
 
-captura.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-captura.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    def reconhecer(self):
+        while not cv2.waitKey(20) & 0xFF == ord("q"):
+            ret, frame = self.captura.read()
+            cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            rostos = self.classificador.detectMultiScale(cinza, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-nomes_salvos, medidas_salvas = buscar_medidas_banco()
+            for (x, y, w, h) in rostos:
+                rostos_rgb = frame [y:y+h, x:x+w]
+                medida_rosto = face_recognition.face_encodings(rostos_rgb)
 
-while not cv2.waitKey(20) & 0xFF == ord("q"):
-    ret, frame = captura.read()
+                nome_identificado = "Desconhecido"
 
-    cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if medida_rosto:
+                    medida_rosto = medida_rosto[0]
 
-    rostos = classificador.detectMultiScale(cinza, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+                    resultados = face_recognition.compare_faces(self.medidas_salvas, medida_rosto, tolerance=0.6)
 
-    for (x, y, w, h) in rostos:
-        rostos_rgb = frame [y:y+h, x:x+w]
-        medida_rosto = face_recognition.face_encodings(rostos_rgb)
+                    for i in range(len(resultados)):
+                        if resultados[i]:
+                            nome_identificado = self.nomes_salvos[i]
+                            break
+                                        
+                cv2.rectangle(frame, (x, y), (x + w, y + w), (0,255,0), 3)
+                cv2.putText(frame, nome_identificado, (x, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
 
-        nome_identificado = "Desconhecido"
+            cv2.imshow("Reconhecimento Facial", frame)
 
-        if medida_rosto:
-            medida_rosto = medida_rosto[0]
-
-            resultados = face_recognition.compare_faces(medidas_salvas, medida_rosto, tolerance=0.6)
-
-            for i in range(len(resultados)):
-                if resultados[i]:
-                    nome_identificado = nomes_salvos[i]
-                    break
-                                
-        cv2.rectangle(frame, (x, y), (x + w, y + w), (0,255,0), 3)
-        cv2.putText(frame, nome_identificado, (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
-
-    cv2.imshow("Reconhecimento Facial", frame)
-
-captura.release()
-cv2.destroyAllWindows()
+        self.captura.release()
+        cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from medidas_rosto import MedidasRosto
+from capturar_rosto import ReconhecimentoFacial
 
 if __name__ == "__main__":
     imagens = MedidasRosto()
     imagens.processar_imagens()
+
+    reconhecimento = ReconhecimentoFacial()
+    reconhecimento.reconhecer()
